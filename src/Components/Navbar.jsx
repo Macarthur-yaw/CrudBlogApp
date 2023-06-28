0510e89cdf4758a38ce4ddd9bcbcc3d4823078d0
Kevin <arthurkevin1260@gmail.com>
2023-06-28 17:44:45 +0100

    added all functionalities

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-
 
 const Navbar = () => {
   const [display, setDisplay] = useState(false);
@@ -8,70 +7,92 @@ const Navbar = () => {
   const [text, setText] = useState("");
   const [content, setContent] = useState("");
   const [source, setSource] = useState("");
+  const [info, setInfo] = useState([]);
+  const [active, setActive] = useState(false);
+  const [updateIndex, setUpdateIndex] = useState(null);
 
-  const initialInput = {
-    text: text,
-    content: content,
-    source: source,
-  };
+  useEffect(() => {
+    const getInfo = JSON.parse(localStorage.getItem("items"));
+    setInfo(getInfo);
+  }, []);
 
   function addBlogs() {
+    const initialInput = {
+      text: text,
+      content: content,
+      source: source,
+    };
+
     setBlogs([initialInput, ...blogs]);
     localStorage.setItem("items", JSON.stringify([initialInput, ...blogs]));
-    // console.log(blogs);
+    setInfo(JSON.parse(localStorage.getItem("items")));
+
+    setText("");
+    setContent("");
+    setSource("");
   }
 
   function deleteTask(indexToDelete) {
-    const updatedBlogs = blogs.filter((blog, index) => index !== indexToDelete);
+    const updatedBlogs = info.filter((_, index) => index !== indexToDelete);
     setBlogs(updatedBlogs);
-    console.log(indexToDelete);
+    setInfo(updatedBlogs);
+    localStorage.setItem("items", JSON.stringify(updatedBlogs));
   }
 
   function changeDisplay() {
     setDisplay(!display);
+    setActive(false);
+    setUpdateIndex(null);
   }
 
   function updateTask(indexOfElement) {
-    const updatedBlogs = blogs.map((blog, index) => {
-      if (indexOfElement === index) {
-        return initialInput;
+    const blogToUpdate = info[indexOfElement];
+    setText(blogToUpdate.text);
+    setContent(blogToUpdate.content);
+    setSource(blogToUpdate.source);
+    setUpdateIndex(indexOfElement);
+    setDisplay(true);
+    setActive(true);
+  }
+
+  function handleUpdate() {
+    const updatedBlogs = info.map((blog, index) => {
+      if (updateIndex === index) {
+        return {
+          text: text,
+          content: content,
+          source: source,
+        };
       }
       return blog;
     });
-    setBlogs(updatedBlogs);
+
+    setInfo(updatedBlogs);
+    localStorage.setItem("items", JSON.stringify(updatedBlogs));
+
+    setText("");
+    setContent("");
+    setSource("");
+    setDisplay(false);
+    setActive(false);
+    setUpdateIndex(null);
   }
 
-//   const getItems = JSON.parse(localStorage.getItem("items"));
-//   console.log(getItems);
-
-  const getData = blogs.map((content, index) => {
-    return (
-      <div key={index}>
-        {content.content}
-        {content.source}
-        {content.text}
-        <button onClick={() => deleteTask(index)}>Delete</button>
-        <button onClick={() => updateTask(index)}>Update</button>
-        <Link to={`/next/${index}`}>Click</Link>
-      </div>
-    );
-  });
-
   return (
-    <div >
-        <h2 className="bg-white-100 shadow-md p-2 text-center ">
-            TechBLOG
-        </h2>
-        {/* welcome */}
-        <div className=" text-end p-2">
-      <button className="border-2 h-8 text-sm border-black p-1 rounded"  onClick={changeDisplay}>Add Blog
-      
-      </button>
+    <div>
+      <h2 className="bg-white-100 shadow-md p-2 text-center">TechBLOG</h2>
+      <div className="text-end p-2">
+        <button
+          className="border-2 h-8 text-sm border-black p-1 rounded"
+          onClick={changeDisplay}
+        >
+          Add Blog
+        </button>
       </div>
       {display && (
-        <div className="flex flex-col border-2 p-2 border-black text-center">
+        <div className="flex flex-col w-96 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 shadow-xl p-2 text-center bg-gray-100">
           <input
-          className="border-2 border-black rounded p-2 text-center"
+            className="border-2 border-black rounded p-2 text-center"
             type="text"
             onChange={(e) => setText(e.target.value)}
             value={text}
@@ -79,7 +100,7 @@ const Navbar = () => {
           />
           <br />
           <input
-          className="border-2 border-black rounded p-2 text-center"
+            className="border-2 border-black rounded p-2 text-center"
             type="text"
             onChange={(e) => setContent(e.target.value)}
             value={content}
@@ -93,14 +114,67 @@ const Navbar = () => {
             value={source}
             placeholder="Content"
           />
-<br/>
-          <button onClick={addBlogs}>Submit</button>
+          <br />
+          <div className="flex flex-row">
+            <button
+              className="border-2 border-black w-20 mx-auto rounded bg-black text-white"
+              onClick={addBlogs}
+            >
+              Submit
+            </button>
+
+            {active && updateIndex !== null ? (
+              <button
+                className="border-2 border-black w-20 mx-auto rounded bg-black text-white"
+                onClick={handleUpdate}
+              >
+                Update
+              </button>
+            ) : (
+              <button className="disabled" disabled>
+                Update
+              </button>
+            )}
+          </div>
         </div>
       )}
-
-      {getData}
+      <div className="p-4">
+        <div className="grid grid-cols-1 gap-4 mt-4">
+          {info.map((content, index) => (
+            <div
+              key={index}
+              className="w-full rounded bg-white-600 p-2 shadow-md"
+            >
+              <h3 className="text-lg font-semibold mb-2">{content.text}</h3>
+              <p className="text-gray-600 mb-2">{content.source}</p>
+              <p className="text-gray-500">{content.content}</p>
+              <div className="flex justify-end mt-4">
+                <button
+                  className="bg-red-500 text-white py-2 px-4 rounded mr-2"
+                  onClick={() => deleteTask(index)}
+                >
+                  Delete
+                </button>
+                <button
+                  className="bg-blue-500 text-white py-2 px-4 rounded"
+                  onClick={() => updateTask(index)}
+                >
+                  Update
+                </button>
+                <Link
+                  className="text-blue-500 underline ml-2"
+                  to={`/next/${index}`}
+                >
+                  Click
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
 
 export default Navbar;
+
--- src/Nex.jsx
+++ src/Nex.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 
 const Nex = () => {
-//   const { id } = useParams();
+  const { id } = useParams();
   const [data, setData] = useState([]);
 
   useEffect(() => {
@@ -10,16 +10,22 @@ const Nex = () => {
     setData(getItems);
   }, []);
 
-  return (
-    <div>
-      <h1>Welcome to the next page</h1>
-      {data.map((item, index) => (
+  const getData = data.map((item, index) => {
+    if (id === index.toString()) {
+      return (
         <div key={index}>
-          <h2>{item.title}</h2>
+          <h3>{item.text}</h3>
           <p>{item.content}</p>
           <p>{item.source}</p>
         </div>
-      ))}
+      );
+    } 
+  });
+
+  return (
+    <div>
+      <h1>Welcome to the next page</h1>
+      {getData}
     </div>
   );
 };
